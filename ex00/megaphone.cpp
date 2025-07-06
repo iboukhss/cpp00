c8b90696f8a9d9ef04a1648c0a4c9ea3eb2f7c8c
Ismail <151830328+iboukhss@users.noreply.github.com>
2025-07-06 23:17:53 +0200

    small fixes in ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 
-static std::string toUppercase(const char* cstr)
+static std::string toUppercase(const std::string& str)
 {
-    std::string res(cstr);
-    for (std::string::iterator it = res.begin(); it != res.end(); ++it) {
+    std::string cpy = str;
+
+    for (std::string::iterator it = cpy.begin(); it != cpy.end(); ++it) {
         *it = std::toupper(static_cast<unsigned char>(*it));
     }
-    return res;
+    return cpy;
 }
 
 int main(int argc, char* argv[])
@@ -17,9 +18,10 @@ int main(int argc, char* argv[])
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
         return 0;
     }
+
     for (int i = 1; i < argc; i++) {
         std::cout << toUppercase(argv[i]);
     }
-    std::cout << '\n';
+    std::cout << "\n";
     return 0;
 }
